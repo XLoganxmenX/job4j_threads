003e859849e94d822c2795c3baa03896845388b7
XLoganxmenX <loganxmen97@gmail.com>
2024-05-23 22:05:17 +0900

    update ConsoleProgress [#1019 #497322]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -5,17 +5,17 @@ public class ConsoleProgress implements Runnable {
     @Override
     public void run() {
         var process = new char[] {'-', '\\', '|', '/'};
-        int index = 0;
         while (!Thread.currentThread().isInterrupted()) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                break;
+            for (char c : process) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.print("\r load: Finished!");
+                    return;
+                }
+                System.out.print("\r load: " + c);
             }
-            System.out.print("\r load: " + process[index]);
-            index = (index + 1) % process.length;
         }
-        System.out.print("\r load: Finished!");
     }
 
     public static void main(String[] args) {

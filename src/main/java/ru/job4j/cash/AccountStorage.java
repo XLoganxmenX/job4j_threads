05e60ca6f292676792c19281d1f12b9d2dcad227
XLoganxmenX <loganxmen97@gmail.com>
2024-06-03 20:16:08 +0900

    change AccountStorage (add, update, transfer methods), add new tests [#1104 #498920]

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -10,25 +10,15 @@ public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
     public boolean add(Account account) {
-        boolean result = false;
         synchronized (accounts) {
-            if (!accounts.containsKey(account.id())) {
-                accounts.put(account.id(), account);
-                result = true;
-            }
+            return accounts.putIfAbsent(account.id(), account) == null;
         }
-        return result;
     }
 
     public boolean update(Account account) {
-        boolean result = false;
         synchronized (accounts) {
-            if (accounts.containsKey(account.id())) {
-                accounts.replace(account.id(), account);
-                result = true;
-            }
+            return accounts.replace(account.id(), accounts.get(account.id()), account);
         }
-        return result;
     }
 
     public void delete(int id) {
@@ -44,26 +34,20 @@ public class AccountStorage {
     }
 
     public boolean transfer(int fromId, int toId, int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount is negative");
+        }
+
         boolean result = false;
+        synchronized (accounts) {
+            Account fromAccount = getById(fromId)
+                    .orElseThrow(() -> new IllegalStateException("Not found account by id = " + fromId));
+            Account toAccount = getById(toId)
+                    .orElseThrow(() -> new IllegalStateException("Not found account by id = " + toId));
 
-        if (amount > 0) {
-            synchronized (accounts) {
-                Account fromAccount = validateAccount(fromId);
-                if (isNegativeBalance(fromAccount)) {
-                    throw new IllegalArgumentException("Operation cannot be performed! "
-                            + "Account balance is less than zero");
-                }
-                Account toAccount = validateAccount(toId);
-
-                int newStateOfAmountFromAccount = fromAccount.amount() - amount;
-                Account newStateFromAccount = new Account(fromId, newStateOfAmountFromAccount);
-
-                if (isNegativeBalance(newStateFromAccount)) {
-                    throw new IllegalArgumentException("Insufficient funds on balance");
-                }
-
-                int newStateOfAmountToAccount = toAccount.amount() + amount;
-                Account newStateToAccount = new Account(toId, newStateOfAmountToAccount);
+            if (fromAccount.amount() >= amount) {
+                Account newStateFromAccount = new Account(fromId, fromAccount.amount() - amount);
+                Account newStateToAccount = new Account(toId, toAccount.amount() + amount);
 
                 update(newStateFromAccount);
                 update(newStateToAccount);
@@ -72,18 +56,5 @@ public class AccountStorage {
         }
 
         return result;
-    }
-
-    private Account validateAccount(int accountId) {
-        Optional<Account> account = getById(accountId);
-        if (account.isEmpty()) {
-            throw new IllegalArgumentException(String.format("Account with id - %d not found", accountId));
-        }
-
-        return account.get();
-    }
-
-    private boolean isNegativeBalance(Account account) {
-        return account.amount() < 0;
     }
 }
--- src/test/java/ru/job4j/cash/AccountStorageTest.java
+++ src/test/java/ru/job4j/cash/AccountStorageTest.java
@@ -16,6 +16,20 @@ class AccountStorageTest {
     }
 
     @Test
+    void whenAddNewAccountThenTrue() {
+        var storage = new AccountStorage();
+        assertThat(storage.add(new Account(1, 100))).isTrue();
+    }
+
+    @Test
+    void whenAddExistAccountThenFalse() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        assertThat(storage.add(new Account(1, 200))).isFalse();
+        assertThat(storage.getById(1).get().amount()).isEqualTo(100);
+    }
+
+    @Test
     void whenUpdate() {
         var storage = new AccountStorage();
         storage.add(new Account(1, 100));
@@ -23,6 +37,20 @@ class AccountStorageTest {
         var firstAccount = storage.getById(1)
                 .orElseThrow(() -> new IllegalStateException("Not found account by id = 1"));
         assertThat(firstAccount.amount()).isEqualTo(200);
+    }
+
+    @Test
+    void whenUpdateSuccessThenTrue() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        assertThat(storage.update(new Account(1, 200))).isTrue();
+    }
+
+    @Test
+    void whenUpdateNotExistsAccountThenFalse() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        assertThat(storage.update(new Account(2, 200))).isFalse();
     }
 
     @Test
@@ -48,22 +76,35 @@ class AccountStorageTest {
     }
 
     @Test
-    void whenTransferNegativeAmount() {
+    void whenTransferMoreAmountThanExistInAccountThenFalse() {
         var storage = new AccountStorage();
         storage.add(new Account(1, 100));
         storage.add(new Account(2, 100));
-        assertThat(storage.transfer(1, 2, -100)).isFalse();
+
+        assertThat(storage.transfer(1, 2, 1000)).isFalse();
+        assertThat(storage.getById(1).get().amount()).isEqualTo(100);
+        assertThat(storage.getById(2).get().amount()).isEqualTo(100);
     }
 
     @Test
-    void whenTransferMoreAmountThanExistInAccount() {
+    void whenTransferInNotExistAccountThenThrowException() {
         var storage = new AccountStorage();
         storage.add(new Account(1, 100));
         storage.add(new Account(2, 100));
-        assertThatThrownBy(() -> storage.transfer(1, 2, 1000))
+
+        assertThatThrownBy(() -> storage.transfer(3, 2, 100))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Not found account by id = 3");
+    }
+
+    @Test
+    void whenTransferNegativeAmountThenThrowException() {
+        var storage = new AccountStorage();
+        storage.add(new Account(1, 100));
+        storage.add(new Account(2, 100));
+
+        assertThatThrownBy(() -> storage.transfer(1, 2, -100))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Insufficient funds on balance");
-        assertThat(storage.getById(1).get().amount()).isEqualTo(100);
-        assertThat(storage.getById(2).get().amount()).isEqualTo(100);
+                .hasMessage("Amount is negative");
     }
 }
